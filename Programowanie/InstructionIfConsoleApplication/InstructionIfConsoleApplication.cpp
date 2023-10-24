e97dff4661d751d42abb6c353a97c36bdabe541f
IronLittleFox <lukasz.felisek@gmail.com>
2023-10-24 09:51:31 +0200

    koniec lekcji

--- Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
+++ Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+//#include <cmath>
+//#include <math.h>
 
 /*
 Operatory warunkowe:
@@ -51,7 +53,7 @@ BMI = masa/wysokość^2
 *. Napisz program, który poprosi użytkownika o podanie długości trzech odcinków i sprawdzi, czy można zbudować z nich trójkąt. Wyświetl odpowiedni komunikat.
 *. Napisz program, który poprosi użytkownika o podanie liczby dodatniej i obliczy jej pierwiastek kwadratowy, jeśli jest to możliwe. Wyświetl wynik lub odpowiedni komunikat, jeśli liczba jest ujemna.
 *. Program sprawdzający czy podana data jest poprawna (np. sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
-*. 
+*.
 
 */
 
@@ -65,8 +67,8 @@ void task1()
 	if (number > 0)
 		std::cout << "Liczba jest dodatnia\n";
 
-	if (number < 0)
-		std::cout << "Liczba jest ujemna";
+	if (number < 0);
+	std::cout << "Liczba jest ujemna";
 
 	if (number == 0)
 		std::cout << "Liczba jest równa zero";
@@ -131,7 +133,7 @@ void task3()
 	//wersja 4
 	if (number < 1 || number >= 10)
 		std::cout << "Liczba z poza zakresu\n";
-	else	
+	else
 		std::cout << "Liczba jest w przedziale\n";
 
 	//wersja 5
@@ -144,7 +146,7 @@ void task3()
 //Napisz program, który wykona dzielenie dwóch liczb
 void task4()
 {
-	int firstNumber,secondNumber;
+	int firstNumber, secondNumber;
 	std::cout << "Podaj liczbe:\n";
 	std::cin >> firstNumber;
 	std::cout << "Podaj liczbe:\n";
@@ -159,7 +161,66 @@ void task4()
 		std::cout << "Dzzielenie przez zero!!!\n";
 }
 
+//Program sprawdzający czy podana data jest poprawna (np. sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
+void task5()
+{
+	int day, month, year;
+	std::cout << "Podaj dzień\n";
+	std::cin >> day;
+	std::cout << "Podaj miesiąc\n";
+	std::cin >> month;
+	std::cout << "Podaj rok\n";
+	std::cin >> year;
+
+	if (day >= 1 && day <= 31
+		&& month >=1 && month <=12
+		&& year != 0
+
+		&& ((month == 4 || month == 6 || month == 9 || month == 11) && day != 31)
+
+		&& month == 2 && (day >=28
+						 || day == 29 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ))
+		)
+	{
+		std::cout << "Data " << day << "." << month << "." << year << " jest poprawna\n";
+	}
+	else
+	{
+		std::cout << "Data nie jest poprawna";
+	}
+}
+
+//Napisz program, który poprosi użytkownika o podanie liczby od 1 do 7 i wyświetli odpowiadający mu dzień tygodnia.
+void task6()
+{
+	int dayOfWeek;
+	std::cout << "Podaj numer dnia tygodnia\n";
+	std::cin >> dayOfWeek;
+
+	if (dayOfWeek == 1)
+		std::cout << "Podniedziałek\n";
+	if (dayOfWeek == 2)
+		std::cout << "Wtorek\n";
+	if (dayOfWeek == 3)
+		std::cout << "Środa\n";
+	if (dayOfWeek == 4)
+		std::cout << "Czwartek\n";
+	if (dayOfWeek == 5)
+		std::cout << "Piątek\n";
+	if (dayOfWeek == 6)
+		std::cout << "Sobota\n";
+	if (dayOfWeek == 7)
+		std::cout << "Niedziela\n";
+
+	if (dayOfWeek < 1 || dayOfWeek > 7)
+		std:cout << "Dzień niepoprawny\n";
+}
+
 int main()
 {
-	task4();
+	task1();
+
+	float number = 9;
+	double root = pow(number, 19) + sqrt(number) + 8;
+
 }
