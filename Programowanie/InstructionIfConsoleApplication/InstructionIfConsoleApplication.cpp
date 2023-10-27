83fb2d0888cc4466c6a7a0e1243c912c6840f3fb
IronLittleFox <lukasz.felisek@gmail.com>
2023-10-27 14:12:40 +0200

    koniec lekcji

--- Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
+++ Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -3,6 +3,8 @@
 //#include <math.h>
 
 /*
+DRY - don't repeat yourself - nie powtarzaj się
+ 
 Operatory warunkowe:
 > - większe
 < - mniejsze
@@ -178,7 +180,7 @@ void task5()
 
 		&& ((month == 4 || month == 6 || month == 9 || month == 11) && day != 31)
 
-		&& month == 2 && (day >= 28
+		&& month == 2 && (day <= 28
 			|| day == 29 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0))
 		)
 	{
@@ -209,10 +211,38 @@ void task6()
 		std::cout << "Piątek\n";
 	else if (dayOfWeek == 6)
 		std::cout << "Sobota\n";
-	else if (dayOfWeek == 7)
+	else if (dayOfWeek == 7 || dayOfWeek == 0)
 		std::cout << "Niedziela\n";
 	else
 		std::cout << "Dzień niepoprawny\n";
+
+	switch (dayOfWeek)
+	{
+	case 1:
+		std::cout << "Podniedziałek\n";
+		break;
+	case 2:
+		std::cout << "Wtorek\n";
+		break;
+	case 3:
+		std::cout << "Środa\n";
+		break;
+	case 4:
+		std::cout << "Czwartek\n";
+		break;
+	case 5:
+		std::cout << "Piątek\n";
+		break;
+	case 6:
+		std::cout << "Sobota\n";
+		break;
+	case 0:
+	case 7:
+		std::cout << "Niedziela\n";
+		break;
+	default:
+		std::cout << "Dzień niepoprawny\n";
+	}
 }
 
 int main()
