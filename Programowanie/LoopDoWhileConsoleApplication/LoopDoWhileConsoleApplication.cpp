157548f52b118c775078dc590db6576c9af23a1b
IronLittleFox <lukasz.felisek@gmail.com>
2023-10-31 09:06:30 +0100

    koniec lekcji

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -32,6 +32,21 @@ void task1()
 	std::cout << "Liczba dodatnia pobrana od uzytkownika " << numberFromUser << "\n";
 }
 
+//Napisz program, który ma pobrać od użytkownika liczbę dodatnią. 
+//Zabezpiecz program przed pobieraniem liczb ujemnych.
+void task2()
+{
+	int numberFromUser;
+
+	do
+	{
+		std::cout << "Podaj liczbę doodatnią:\n";
+		std::cin >> numberFromUser;
+	} while (numberFromUser < 0);
+
+	std::cout << "Liczba dodatnia pobrana od uzytkownika " << numberFromUser << "\n";
+}
+
 int main()
 {
     
