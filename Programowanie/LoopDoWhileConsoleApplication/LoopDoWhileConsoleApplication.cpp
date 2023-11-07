355a5c12b817ec7b1d1306823e6a6b4b0b1d61f6
IronLittleFox <lukasz.felisek@gmail.com>
2023-11-07 09:53:15 +0100

    koniec lekcji

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -5,6 +5,7 @@
 void task1()
 {
 	int numberFromUser;
+
 	std::cout << "Podaj liczbę doodatnią:\n";
 	std::cin >> numberFromUser;
 	if (numberFromUser < 0)
@@ -40,15 +41,55 @@ void task2()
 
 	do
 	{
-		std::cout << "Podaj liczbę doodatnią:\n";
+		std::cout << "Podaj liczbę dodatnią:\n";
 		std::cin >> numberFromUser;
 	} while (numberFromUser < 0);
 
 	std::cout << "Liczba dodatnia pobrana od uzytkownika " << numberFromUser << "\n";
 }
 
+//Napisz program, który wylosuje liczbę a następnie uzytkownik będzie musiał ją zgadnąć.
+void task3()
+{
+	const int LOWER_RANGE = 1;
+	const int UPPER_RANGE = 100;
+	srand(time(NULL));
+	int randomNumber = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+	//std::cout << randomNumber << "\n";
+
+	int numberFromUser;
+
+	/*
+	std::cout << "Podaj liczbę:\n";
+	std::cin >> numberFromUser;
+	if (numberFromUser != randomNumber)
+	{
+		std::cout << "Podaj liczbę:\n";
+		std::cin >> numberFromUser;
+		if (numberFromUser != randomNumber)
+		{
+			std::cout << "Podaj liczbę:\n";
+			std::cin >> numberFromUser;
+			//...
+		}
+	}*/
+
+	do
+	{
+		std::cout << "Podaj liczbę:\n";
+		std::cin >> numberFromUser;
+		if (numberFromUser > randomNumber)
+			std::cout << "Za duża liczba\n";
+		if (numberFromUser < randomNumber)
+			std::cout << "Za mała liczba\n";
+	} while (numberFromUser != randomNumber);
+
+	std::cout << "Gratulacje!!!!\n";
+
+}
+
 int main()
 {
-    
+	task3();
 
 }
