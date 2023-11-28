fdc84e09d28f182ac5732f26291cf172fcd12d78
IronLittleFox <lukasz.felisek@gmail.com>
2023-11-28 09:52:32 +0100

    koniec lekcji

--- Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
+++ Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
@@ -53,12 +53,12 @@ void task2()
 	std::cin >> secondNumber;
 
 	int nwd;
-
+	/*
 	if (firstNumber < secondNumber)
 		nwd = firstNumber;
 	else
 		nwd = secondNumber;
-
+	*/
 	nwd = (firstNumber < secondNumber) ? firstNumber : secondNumber;
 
 	/*
@@ -80,6 +80,7 @@ void task2()
 	}
 	*/
 
+	//wersja 1
 	while (secondNumber % nwd != 0
 		|| firstNumber % nwd != 0)
 	{
@@ -87,6 +88,75 @@ void task2()
 	}
 
 	std::cout << "NWD = " << nwd << "\n";
+
+	//wersja 2
+	nwd = 1;
+	int dividend = 2;
+	int tmpFirstNumber = firstNumber, tmpSecondNumber = secondNumber;
+	while (tmpFirstNumber >= dividend
+		&& tmpSecondNumber >= dividend)
+	{
+		if (tmpFirstNumber % dividend == 0
+			&& tmpSecondNumber % dividend == 0)
+		{
+			tmpFirstNumber = tmpFirstNumber / dividend;
+			tmpSecondNumber /= dividend;
+			nwd *= dividend;
+		}
+		else
+			dividend++;
+	}
+	std::cout << "NWD = " << nwd << "\n";
+
+	//wersja 3
+	//NWD(a, b) = a				jeśli b = 0
+	//NWD(a, b) = NWD(b, a % b) jeśli b != 0
+	int a = firstNumber, b = secondNumber;
+	while (b != 0)
+	{
+		int tmpA = a;
+		a = b;
+		b = tmpA % b;
+	}
+	nwd = a;
+	std::cout << "NWD = " << nwd << "\n";
+}
+
+//Sprawdzanie czy liczba jest palindromem.
+void task3()
+{
+	int number;
+	std::cout << "Podaj liczbę\n";
+	std::cin >> number;
+
+	//wersja 1
+
+	//obliczam ilość cyfr
+	int tmpNumber = number;
+	int numberOfDigit = 1;
+	while (tmpNumber >= 10)
+	{
+		numberOfDigit++;
+		tmpNumber = tmpNumber / 10;
+	}
+
+	//liczę 10 do potęgi numberOfDigit - 1
+	int leftDivided = 1;
+	while (numberOfDigit != 1)
+	{
+		leftDivided *= 10;
+		numberOfDigit--;
+	}
+
+	int rightDivided = 10;
+	int leftNumber = number;
+	int rightNumber = number;
+
+	while (leftNumber > 10)
+	{
+
+	}
+
 }
 
 int main()
