6f59433139058fce4a2097698ce83bba4539a038
IronLittleFox <lukasz.felisek@gmail.com>
2023-12-19 08:43:27 +0100

    dodanie zadań

--- Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
+++ Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -33,10 +33,57 @@ void task2()
 
 }
 
+//Napisz program, który poprosi cie o twoje imię i cię przywita.
+void task3()
+{
+    std::string userName;
+    std::cout << "Podaj swoje imie\n";
+    std::cin >> userName;
+
+    std::cout << "Witaj " << userName << " tutaj.\n";
+}
+
+//Program sprawdzający czy podane hasło jest poprawne 
+//(np. jeśli hasło jest "abc123", program powinien wyświetlić "hasło poprawne", 
+//jeśli jest inne, powinien wyświetlić "hasło niepoprawne").
+void task4()
+{
+    std::string password;
+    std::cout << "Podaj hasło\n";
+    std::cin >> password;
+
+    if (password == "abc123")
+        std::cout << "Hasło poprawne\n";
+    else
+        std::cout << "Hasło niepoprawne\n";
+}
+
+//Napisz program, który wczyta łańcuch znaków i policzy ile jest małych liter 'a'.
+void task5()
+{
+    std::string textFromUser;
+    std::cout << "Podaj łańcuch znaków\n";
+    std::cin >> textFromUser;
+
+    int numberOfCharacters = 0;
+
+    //std::cout << "Pierwszy znak to " << textFromUser[0] << "\n";
+    //textFromUser[1] = 'x';
+    //std::cout << "Ilość znaków " << textFromUser.length()  << "\n";
+    
+    for (int i = 0; i < textFromUser.length(); i++)
+    {
+        if (textFromUser[i] == 'a')
+            numberOfCharacters++;
+    }
+
+    std::cout << "Małych liter 'a' jest " << numberOfCharacters << "\n";
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "polish");
-    task2();
+    task5();
 }
 
 /*
