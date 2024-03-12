1c52936e2cc0b5af09bb6e31c96cc7186b4ae2a5
g <g@g.pl>
2024-03-12 07:45:45 +0100

    dodane zadania

---
+++ Programowanie/TasksConsoleApplication/TasksConsoleApplication.cpp
@@ -0,0 +1,36 @@
+﻿// TasksConsoleApplication.cpp
+
+#include <iostream>
+
+/*
+Zadanie: Tworzenie prostego kalkulatora
+
+Napisz program, który będzie działał jako prosty kalkulator 
+i będzie umożliwiał użytkownikowi wykonywanie 
+podstawowych operacji arytmetycznych na dwóch liczbach.
+
+Instrukcje:
+
+Poproś użytkownika o podanie dwóch liczb oraz wybór operacji (dodawanie, odejmowanie, mnożenie lub dzielenie).
+W zależności od wyboru użytkownika, wykonaj odpowiednią operację arytmetyczną.
+Wyświetl wynik operacji.
+Przykładowe działanie programu:
+
+Prosty Kalkulator
+-----------------
+1. Dodawanie
+2. Odejmowanie
+3. Mnożenie
+4. Dzielenie
+
+Wybierz operację (1/2/3/4): 3
+Podaj pierwszą liczbę: 5
+Podaj drugą liczbę: 3
+
+Wynik mnożenia: 15
+*/
+
+int main()
+{
+    
+}
